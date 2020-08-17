c41f7e0dd888ea25f793457f2b8f8270b5b4d10b
TitanTigger <63661630+TitanTigger@users.noreply.github.com>
2020-08-17 11:47:04 +0200

    First Commit

---
+++ M226B/M226B_Autovermietung/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M226B_Autovermietung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!2");
+        }
+    }
+}
